935eabaca06e35e3ebf9a0faa818162c67e4462f
Francisco Dans <fdans@wikimedia.org>
2018-04-04 14:40:43 +0200

    Adds/fixes touch interactions in graphs
    Adds tooltip UI to the bar chart to make it possible to see exact values
    in that graph, including on mobile view. It also fixes the line graph guide
    line so that it follows the mouse pointer correctly.
    
    Bug: T188277
    Change-Id: I00c426284abfb4948dbd26c01e08c52ee5fde2f7

--- src/components/detail/chart/BarChart.vue
+++ src/components/detail/chart/BarChart.vue
@@ -1,5 +1,20 @@
 <template>
-<div class="big bar chart">
+<div class="graphContainer">
+    <div v-if="hoveredBar" class="bar valuePopup" :style="getPopupPosition(hoveredBar)" :class="popupOrientation(hoveredBar)">
+        <b>{{hoveredBar.month | ISOdateUTC }}</b>
+        <div><b><span :style="{ color: hoveredBar.color }">{{hoveredBar.key}}</span></b>
+        <span>{{hoveredBar.value | thousands}}</span></div>
+    </div>
+    <div class="big bar chart">
+        <svg>
+            <pattern id="diagonalHatch" patternUnits="userSpaceOnUse" width="4" height="8" patternTransform="rotate(45 0 0)">
+                <rect x="0" y="0" width="50" height="50"/>
+                <line x1="0" y1="0" x2="0" y2="50" style="stroke:black; stroke-width:3" />
+            </pattern>
+            <g class="graph">
+            </g>
+        </svg>
+    </div>
 </div>
 </template>
 
@@ -18,6 +33,12 @@ export default {
     name: 'bar-chart',
 
     props: ['graphModel', 'data'],
+
+    data () {
+        return {
+            hoveredBar: null
+        };
+    },
 
     mounted () {
         this.drawChart();
@@ -49,15 +70,18 @@ export default {
                 unitFilter = this.$options.filters.kmb;
             }
 
-            const root = d3.select(this.$el),
+            // We make sure that any selected point in a previous chart is cleared
+            this.hoveredBar = null;
+
+            const root = d3.select(this.$el).select('.big'),
                   margin = {top: 6, right: 0, bottom: 20, left: 40},
                   padding = 4;
 
-            // clean out any previous chart
-            root.selectAll('*').remove();
+            const svg = root.select('svg'),
+                  g = svg.select('g');
 
-            const svg = root.append('svg'),
-                  g = svg.append('g');
+            // clean out any previous chart
+            g.selectAll('*').remove();
 
             const n = root.node();
             const activeDict = this.graphModel.getActiveBreakdownValues();
@@ -92,6 +116,7 @@ export default {
             let graphElement = g.append('g');
 
             y.domain([min, max]);
+            let self = this;
             graphElement.selectAll('.bar').data(this.data)
                 .enter().selectAll('.minibar').data((d) => {
                     const newData = this.graphModel.activeBreakdown.values
@@ -119,7 +144,22 @@ export default {
                     })
                     .attr('width', (d) => d.width)
                     .attr('height', (d) => Math.abs(y(d.value) - y(0)))
-                    .attr('fill', (d) => d.color);
+                    .attr('fill', (d) => d.color)
+                    .on('mouseover', function (d) {
+                        const diagonalHatch = d3.select('#diagonalHatch')
+                        d3.select(diagonalHatch.node().firstChild).style('fill', d.color);
+                        d3.select(this).attr('fill', 'url(#diagonalHatch)');
+                        self.hoveredBar = d;
+                        const containerBB = self.$el.getBoundingClientRect();
+                        const barBB = this.getBoundingClientRect();
+                        self.hoveredBar.left = barBB.left - containerBB.left + margin.left;
+                        self.hoveredBar.top = barBB.top - containerBB.top + barBB.height;
+                        self.hoveredBar.right = containerBB.right - barBB.right + margin.left;
+                    })
+                    .on('mouseout', function (d) {
+                        d3.select(this).attr('fill', d.color);
+                        d3.event.stopPropagation();
+                    });
 
             if (min < 0) {
                 graphElement.append('line')
@@ -145,8 +185,30 @@ export default {
                     .attr("dy", ".15em")
                     .attr("transform", "rotate(-45)");
             svg.attr('width', n.offsetWidth).attr('height', g.node().getBBox().height + margin.top);
+            // n.onmouseout = this.onGraphMouseOut.bind(this);
 
         },
+        onGraphMouseOut (e) {
+            this.hoveredBar = null;
+        },
+        popupOrientation (bar) {
+            if (bar.left > this.$el.getBoundingClientRect().width/2) {
+                return {
+                    'left': true
+                }
+            } else {
+                return {
+                    'right': true
+                }
+            }
+        },
+        getPopupPosition (bar) {
+            if (bar.left > this.$el.getBoundingClientRect().width / 2) {
+                return {top: (bar.top / 2) + 'px', right: bar.right + 'px'}
+            } else {
+                return {top: (bar.top / 2) + 'px', left: bar.left + 'px'}
+            }
+        }
     }
 }
 </script>
@@ -155,6 +217,33 @@ export default {
 .fullscreen .big.bar.chart { min-height: 492px; }
 .big.bar.chart {
     min-height: 386px;
+}
+.bar.valuePopup {
+    text-align: right;
+    position: absolute;
+    background-color:rgba(255, 255, 255, 0.7);
+    padding: 5px;
+    min-width: 20%;
+}
+.bar.valuePopup:after{
+    position: absolute;
+    top: 30%;
+    content: '';
+    width: 0;
+    height: 0;
+    
+}
+.bar.valuePopup.right:after {
+    left: -10px;
+    border-right: solid 10px rgba(255, 255, 255, 0.7);
+    border-bottom: solid 10px transparent;
+    border-top: solid 10px transparent;
+}
+.bar.valuePopup.left:after {
+    right: -10px;
+    border-left: solid 10px rgba(255, 255, 255, 0.7);
+    border-bottom: solid 10px transparent;
+    border-top: solid 10px transparent;
 }
 @media(max-width: 450px) {
     .big.bar.chart {
