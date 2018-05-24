b0767a96241fc7c49f216ee8259e584cbe708242
Sahil Khokhar <sahilkhokhar505@gmail.com>
2018-05-24 23:33:51 +0530

    Upgraded footer UI/design
    Bug: T191672
    Change-Id: I259b9fedf3f72c5c64e60ccc5a5bcb1b4cc2892e

--- src/components/BottomFooter.vue
+++ src/components/BottomFooter.vue
@@ -1,20 +1,40 @@
 <template>
-<section class="ui centered grid">
-    <div class="four wide middle aligned column">
-        Find out more <a target="_blank" href="https://wikitech.wikimedia.org/wiki/Analytics/Wikistats2.0">about Wikistats</a><br/>
-        and the <a target="_blank" href="https://wikitech.wikimedia.org/wiki/Analytics#Datasets">data it uses</a><br/>
+<section class="page-footer">
+    <div class="img-container">
+        <img class="wikimedia-logo" src="https://upload.wikimedia.org/wikipedia/commons/f/f3/Wikimedia-logo-V.svg"
+        alt="wikimedia-logo">
     </div>
-    <div class="eight wide middle aligned column">
-        See our <a target="_blank" href="//wikitech.wikimedia.org/wiki/Analytics/Wikistats2.0#Contributing_and_Deployment">contributing guide</a> to help us build Wikistats<br/>
-        We use <a target="_blank" href="//wikitech.wikimedia.org/wiki/Analytics/Wikistats2.0#Technology">Vue.js, d3, CrossFilter, and more</a><br/>
-        Follow this link to <a target='_blank' href='https://phabricator.wikimedia.org/maniphest/task/edit/?title=Wikistats%20Bug&amp;projectPHIDs=Analytics-Wikistats,Analytics'>report a bug</a><br/>
+    <div class="list-container list-documentation">
+        <h5 class="list-heading">Documentation</h5>
+        <ul class="list-content">
+            <li><a target="_blank" href="https://wikitech.wikimedia.org/wiki/Analytics/Wikistats2.0">About Wikistats</a></li>
+            <li><a target="_blank" href="https://wikitech.wikimedia.org/wiki/Analytics/Systems/Wikistats_2#Technology">Technology</a></li>
+            <li><a target="_blank" href="https://wikitech.wikimedia.org/wiki/Analytics#Datasets">Data it uses</a></li>
+        </ul>
     </div>
-    <div class="four wide middle aligned column">
-        Designed by <a target="_blank" href="//aislinngrigas.com/about/">Aislinn Grigas</a>.<br/>
+    <div class="list-container list-contributing">
+        <h5 class="list-heading">Contributing</h5>
+        <ul class="list-content">
+            <li><a target="_blank" href="https://wikitech.wikimedia.org/wiki/Analytics/Systems/Wikistats_2#Contributing_and_Deployment">Contributing guide</a></li>
+            <li><a target="_blank" href="https://phabricator.wikimedia.org/maniphest/task/edit/?title=Wikistats%20Bug&amp;projectPHIDs=Analytics-Wikistats,Analytics">Report a bug</a></li>
+            <li><a target="_blank" href="https://phabricator.wikimedia.org/maniphest/task/edit/?title=Wikistats%20New%20Feature&projectPHIDs=Analytics-Wikistats,Analytics">New feature</a></li>
+        </ul>
     </div>
-    <div class="row">
-        <div class="sixteen wide middle aligned column">
-            All data, charts, and other content is available under the <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/">Creative Commons CC0 dedication.</a><br/>
+    <div class="list-container list-community">
+        <h5 class="list-heading">Community</h5>
+        <ul class="list-content">
+            <li><a target="_blank" href="https://phabricator.wikimedia.org/tag/analytics-wikistats/">Phabricator</a></li>
+            <li><a target="_blank" href="https://github.com/wikimedia/analytics-wikistats2">Github</a></li>
+            <li><a target="_blank" href="https://webchat.freenode.net/?channels=#wikimedia-analytics">IRC</a></li>
+        </ul>
+    </div>
+    <div class="bottom-footer-container">
+        <div class="credit">
+            Designed by <a target="_blank" href="http://aislinngrigas.com/about/"><span class="base-footer">Aislinn Grigas</span></a>
+        </div>
+        <div class="copyright">
+            All data, charts, and other content is available under the
+            <a target="_blank" href="https://creativecommons.org/publicdomain/zero/1.0/"><span class="base-footer">Creative Commons CC0 dedication</span></a>.
         </div>
     </div>
 </section>
@@ -32,6 +52,138 @@ export default {
 </script>
 
 <style scoped>
-a, a:visited { color: #888; }
-.ui.centered { font-size: 1.1em; }
+
+a {
+    color: #e6e6e7;
+    font-weight: 300;
+}
+
+a:hover {
+    color: #ffffff;
+}
+
+.page-footer {
+    display: grid;
+    grid-gap: 20px;
+    grid-template-areas:
+      'img-container list-documentation list-contributing list-community'
+      'bottom-footer-container bottom-footer-container bottom-footer-container bottom-footer-container';
+    color: #ffffff;
+    font-weight: 300;
+    font-family: sans-serif;
+    line-height: 1.5;
+    margin-left: -1rem;
+    margin-right: -1rem;
+    margin-bottom: -1rem;
+}
+
+.img-container {
+    grid-area: img-container;
+    padding: 1rem 5rem;
+    margin-right: 100px;
+}
+
+.list-documentation {
+    grid-area: list-documentation;
+    padding: 1rem;
+}
+
+.list-contributing {
+    grid-area: list-contributing;
+    padding: 1rem;
+}
+
+.list-community {
+    grid-area: list-community;
+    padding: 1rem;
+}
+
+.bottom-footer-container {
+    grid-area: bottom-footer-container;
+    display: flex;
+    overflow: hidden;
+    background-color: rgba(0, 0, 0, 0.2);
+    color: rgba(255, 255, 255, 0.6);
+    padding: 1rem 2rem;
+}
+
+.wikimedia-logo{
+    width: 100px;
+    height: 100px;
+}
+
+.list-container {
+    padding-bottom: 0;
+}
+
+.list-heading {
+    font-weight: 400;
+    font-size: 1.3rem;
+}
+
+.list-content {
+    list-style: none;
+    padding: 0;
+    line-height: 1.7;
+}
+
+.copyright {
+    margin-left: 200px;
+}
+
+@media(max-width: 767px) {
+    .page-footer {
+        grid-template-areas:
+          'img-container img-container img-container'
+          'list-documentation list-contributing list-community'
+          'bottom-footer-container bottom-footer-container bottom-footer-container';
+    }
+
+    .list-container {
+      margin: auto;
+    }
+
+    .img-container {
+        display: block;
+        margin: auto;
+    }
+}
+
+@media(max-width: 500px) {
+    .page-footer {
+        grid-template-areas:
+          'img-container img-container'
+          'list-documentation list-contributing'
+          'list-community list-community'
+          'bottom-footer-container bottom-footer-container';
+    }
+
+    .list-documentation {
+        display: block;
+        margin: auto;
+    }
+
+    .list-contributing {
+        display: block;
+        margin: auto;
+    }
+
+    .list-community  {
+        display: block;
+        margin: auto;
+    }
+
+    .bottom-footer-container {
+        display: flex;
+        flex-direction: column;
+        margin: auto;
+        text-align: center;
+        justify-content: center;
+    }
+
+    .copyright {
+        margin: auto;
+    }
+}
+
 </style>
